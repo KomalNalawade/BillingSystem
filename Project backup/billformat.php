80cb395532fc25ec000d8d880ec7a674fb28c44c
KomalNalawade <knalawade30@gmail.com>
2018-06-24 07:32:24 +0530

    Added project

---
+++ Project backup/billformat.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bill</title>
+	<style type="text/css">
+		div{
+			background-color: #f1f1f1; 
+			width:40%;
+			padding-left: 50px;
+		}
+		.div1 label
+		{
+
+			position: absolute;			
+
+		}
+	</style>
+</head>
+	<body>
+	<center>
+	<div class="div1">
+		<h1>Hotel Name</h1>
+
+		<br>
+		<br>
+		<br>
+		<label>Customer Name:</label><input type="text" name="custname" style="margin-left: 8em;">
+		<br>
+		<br>
+		<label>Table Name:</label><input type="text" name="tabname" style="margin-left: 8em;">
+
+		<br>
+		<br>
+		<br>
+		<label>Bill</label>
+		<br>
+		<br>
+		<table border="1px solid black" cellpadding="12px" >
+			<tr>
+				<td>Items</td>
+				<td>Quantity</td>
+				<td>Price</td>
+			</tr>
+
+			<tr>
+			<td></td>	
+			<td></td>	
+			<td></td>	
+			</tr>
+
+			<tr>
+				<td></td>
+				<td></td>	
+				<td></td>	
+			</tr>
+
+			<tr>
+				<td></td>	
+				<td></td>	
+				<td></td>	
+			</tr>
+			
+			<tr >
+				<td colspan=3>Total</td>
+			</tr>
+		</table>
+	</div>
+	</center>
+	</body>
+</html>
